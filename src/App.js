6380bf5318f12f6839fab8d3d57b4e7f6ae02e6c
Jagadeesh Varma <jagadeesh@dunderdog.se>
2020-10-09 21:03:17 +0200

    Hello World

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <body>
+      <div class="container">
+          <h1>Alright world time to take you on!</h1>
+        <div class="loader"></div>
+        <p>Welcome to Dunderdog</p>
+        <br/>
+        <p>Our new site will launch very soon</p>
+        <p>Check back here later</p>
+        <br/>
+        <a href="https://dunderdog.se">dunderdog.se</a>
+      </div>
+      </body>
+      </header>
+    </div>
+  );
+}
+
+export default App;
