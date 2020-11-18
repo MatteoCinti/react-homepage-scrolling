23702aab32e13507c687257ebbdeceb4511d983e
MatteoCinti <matteocinti@hotmail.com>
2020-11-18 16:13:24 +0100

    Routing Initial Setup

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './homepage.styles.scss'
+
+const HomePage = () => (
+    <div>
+        <h1 className='test'> TEEEEEEST </h1>
+    </div>
+)
+
+export default HomePage
