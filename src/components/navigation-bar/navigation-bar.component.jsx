e8badcf2a6f3a7fe65e7fc13eec9d5e8069d4830
Ameneh Sahebhonar <amysahr@users.noreply.github.com>
2020-12-18 13:33:09 +0100

    add Link to Logo

--- src/components/navigation-bar/navigation-bar.component.jsx
+++ src/components/navigation-bar/navigation-bar.component.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Menu from '../menu/menu.component'
-import MenuToggle from '../menu-toggle/menu-toggle.component'
-import { ReactComponent as Logotype } from '../../assets/logo/dunderdog.svg'
+import Menu from "../menu/menu.component";
+import MenuToggle from "../menu-toggle/menu-toggle.component";
+import { ReactComponent as Logotype } from "../../assets/logo/dunderdog.svg";
 
-import './navigation-bar.styles.scss'
+import "./navigation-bar.styles.scss";
 
 const NavigationBar = () => (
   <div className="navigation-bar">
-    <Logotype className='logotype'/>
+    <Link to="/">
+      <Logotype className="logotype" />
+    </Link>
     <Menu />
     <MenuToggle />
   </div>
-)
+);
 
-export default NavigationBar
+export default NavigationBar;
